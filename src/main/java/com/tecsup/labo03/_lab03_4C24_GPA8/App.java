3064eb03fb40f4ae3000181408dfe8fb6e0cb5d8
Jossef Muñoz Chavez <mcjossef@gmail.com>
2024-03-27 19:51:46 -0500

    First commit

---
+++ src/main/java/com/tecsup/labo03/_lab03_4C24_GPA8/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.labo03._lab03_4C24_GPA8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
